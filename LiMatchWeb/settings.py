fed99c06b8fc54c2a4138615f905d737e73f701d
ellen.giacometti <ellengiacometti@accenture.com>
2020-06-10 17:28:53 -0300

    editing Procfile

--- LiMatchWeb/settings.py
+++ LiMatchWeb/settings.py
@@ -12,10 +12,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# This is new:
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -47,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'LiMatchWeb.urls'
@@ -78,21 +84,23 @@ WSGI_APPLICATION = 'LiMatchWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+DATABASES = {}
 
-'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'postgres',
-    'USER': 'postgres',
-    'PASSWORD': 'postgres',
-    'HOST': 'limatch.cvixklymkeit.us-east-2.rds.amazonaws.com',
-    'PORT': '5432',
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# 'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'postgres',
+#     'USER': 'postgres',
+#     'PASSWORD': 'postgres',
+#     'HOST': 'limatch.cvixklymkeit.us-east-2.rds.amazonaws.com',
+#     'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -130,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -139,3 +147,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
+
