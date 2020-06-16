9123f8632ac3a61b5d03f2a460fc2f4d45c95bc6
ellen.giacometti <ellengiacometti@accenture.com>
2020-06-16 01:21:54 -0300

    Inserindo label atraves do site

--- lima/views.py
+++ lima/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Pessoa, Amostra
+from .models import Pessoa, Amostra, Label
 import random
 
 def get_random_filenames(n):
     max_id = Amostra.objects.latest('id').id
     randomList = random.sample(range(1, max_id), n)
     filenames = Amostra.objects.values_list('amostra', flat=True).filter(id__in= randomList)
-    return filenames
+    dict_amostra = list(Amostra.objects.values('id','amostra').filter(id__in=randomList))
+    return filenames,dict_amostra
 
 # Create your views here.
 
@@ -30,7 +31,8 @@ def classificar(request):
             info_pessoa.save()
             print("inseriu!")
         request.session['id_pessoa']=Pessoa.objects.get(email=email).pk
-        filenames = get_random_filenames(2)
+        filenames,dict_amostra = get_random_filenames(2)
+        request.session['dict_amostra'] = dict_amostra
         return render(request, 'classificar.html', {'amostras': filenames, 'primeiro': primeiro})
     else:
         print("Preciso de um email")
@@ -40,10 +42,20 @@ def classificar(request):
 def registrar(request):
     primeiro = False
     idpessoa=request.session.get('id_pessoa')
-
-    print(idpessoa)
+    dict_amostra=request.session.get('dict_amostra')
+    for aux in range(len(dict_amostra)):
+          label=(request.POST.get(dict_amostra[aux].get('amostra')))
+          if label is None:
+              print("Esqueceu de preencher!")
+          else:
+              print(label[0],label[1],dict_amostra[aux].get('id'),idpessoa)
+              info_label=Label(maturacao=label[0],defeito=label[1],amostra_id=dict_amostra[aux].get('id'),pessoa_id=idpessoa)
+              info_label.save()
+              print("Salvei")
+                # print(dict_amostra[aux].get('id'),dict_amostra[aux].get('amostra'))
     if 'finalizar' in request.POST:
         return render(request, 'agradecimento.html')
     elif 'mais' in request.POST:
-        filenames = get_random_filenames(4)
-        return render(request, 'classificar.html', {'amostras': filenames, 'primeiro': primeiro})
+        filenames_novos, dict_amostra_novo = get_random_filenames(4)
+        request.session['dict_amostra'] = dict_amostra_novo
+        return render(request, 'classificar.html', {'amostras': filenames_novos, 'primeiro': primeiro})
